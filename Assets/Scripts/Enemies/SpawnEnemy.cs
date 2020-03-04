14b0a85c0505c1873381a139575bb82868a9281c
James <JG228250@falmouth.ac.uk>
2020-03-04 16:55:26 +0000

    Made enemies spawn randomly. made enemy spawn rate random also.

--- Assets/Scripts/Enemies/SpawnEnemy.cs
+++ Assets/Scripts/Enemies/SpawnEnemy.cs
@@ -6,18 +6,31 @@ public class SpawnEnemy : MonoBehaviour
 {
     private float time = 0.0f;
     public Transform enemySpawner;
+
+    public Transform redEnemy;
+    public Transform greenEnemy;
+    public Transform purpleEnemy;
+    public Transform blueEnemy;
+
     // Start is called before the first frame update
     void Start()
     {
 
     }
-
     // Update is called once per frame
     void Update()
     {
+        Random rnd = new Random();
         time += Time.deltaTime;
-        if (time >= 1.0f)
+        if (time >= Random.Range(1, 11))
         {
+            Transform[] enemies = {
+            redEnemy,
+            greenEnemy,
+            purpleEnemy,
+            blueEnemy
+            };
+            enemySpawner = enemies[Random.Range(0, 4)];
             Instantiate(enemySpawner, transform.position, transform.rotation);
             time = 0.0f;
         }
