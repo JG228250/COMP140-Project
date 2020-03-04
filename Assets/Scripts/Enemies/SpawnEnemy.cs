82009b96d08bccf28df37b8892703de811d704fd
James <JG228250@falmouth.ac.uk>
2020-03-04 02:00:58 +0000

    Added enemy spawner. Added temporary enemy sprites. Added movement to enemies.

---
+++ Assets/Scripts/Enemies/SpawnEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    private float time = 0.0f;
+    public Transform enemySpawner;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > 1.0f)
+        {
+            Instantiate(enemySpawner, transform.position, transform.rotation);
+            time = 0.0f;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/SpeakerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeakerController : MonoBehaviour
+{
+    private float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.Rotate(new Vector3(0, 0, -10 * speed * Time.deltaTime));
+        }
+        if(Input.GetKey("s"))
+        {
+            transform.Rotate(new Vector3(0, 0, +10 * speed * Time.deltaTime));
+        }
+    }
+}
