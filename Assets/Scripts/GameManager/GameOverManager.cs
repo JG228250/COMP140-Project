75fe8262a6b0fd698ffb78b84ea19c6f77ac9bcb
James <JG228250@falmouth.ac.uk>
2020-04-18 20:28:38 +0100

    Added high score to game over screen. Added playerpref functionality to playerscore script for high score.

--- Assets/Scripts/GameManager/GameOverManager.cs
+++ Assets/Scripts/GameManager/GameOverManager.cs
@@ -9,11 +9,23 @@ public class GameOverManager : MonoBehaviour
     public float gameOverDelay = 3f;
 
     public Text scoreText;
+    public Text highScoreText;
     // Start is called before the first frame update
     void Start()
     {
         scoreText.text = PlayerScore.playerScore.ToString();
+        highScoreText.text = PlayerPrefs.GetInt("highScore", 0).ToString();
+        SetHighScore();
         Invoke("LoadMainGame", gameOverDelay);
+    }
+
+    public void SetHighScore()
+    {
+        if (PlayerScore.playerScore > PlayerPrefs.GetInt("highScore", 0))
+        {
+            PlayerPrefs.SetInt("highScore", PlayerScore.playerScore);
+            highScoreText.text = PlayerScore.playerScore.ToString();
+        }
     }
 
     void LoadMainGame()
