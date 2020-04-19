2457531250ab206a18a5170f4dab840f907c3c4a
James <JG228250@falmouth.ac.uk>
2020-04-20 00:17:30 +0100

    Final version of project. Arduino controls are functional, however keyboard directional keys and the "A" and "D" keys also work as inputs.

--- Assets/Scripts/GameManager/ReadArduinoStrings.cs
+++ Assets/Scripts/GameManager/ReadArduinoStrings.cs
@@ -1,13 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 using Uduino;
 
 public class ReadArduinoStrings : MonoBehaviour
 {
-    private bool shotFired = false;
-
+    //Read potentiometer value.
+    public static int potValue = 512;
+    //Stores input of the four strings from vibration sensors.
     public int sensorValueE;
     public int sensorValueA;
     private int sensorValueD;
@@ -16,6 +14,8 @@ public class ReadArduinoStrings : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Initiates pins with sensors.
+        UduinoManager.Instance.pinMode(AnalogPin.A0, PinMode.Input);
         UduinoManager.Instance.pinMode(2, PinMode.Input_pullup);
         UduinoManager.Instance.pinMode(3, PinMode.Input_pullup);
         UduinoManager.Instance.pinMode(4, PinMode.Input_pullup);
@@ -25,10 +25,13 @@ public class ReadArduinoStrings : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Reads values from sensors.
+        potValue = UduinoManager.Instance.analogRead(AnalogPin.A0);
         sensorValueE = UduinoManager.Instance.digitalRead(2);
         sensorValueA = UduinoManager.Instance.digitalRead(3);
         sensorValueD = UduinoManager.Instance.digitalRead(4);
         sensorValueG = UduinoManager.Instance.digitalRead(5);
+        //Shoots the weapon based on string input.
         if (sensorValueE == 0)
         {
             FindObjectOfType<SpeakerShooter>().RedWeapon();
--- Assets/Scripts/Player/SpeakerController.cs
+++ Assets/Scripts/Player/SpeakerController.cs
@@ -4,10 +4,23 @@ public class SpeakerController : MonoBehaviour
 {
     //Rotation speed for the speaker, (speed to move 20 degrees).
     private float speed = 5.0f;
-    
+    private int rotateValue;
     // Update is called once per frame
     void Update()
     {
+        //Retrievs the value of the potentiometer.
+        rotateValue = ReadArduinoStrings.potValue;
+        //Rotates the speaker clockwise/
+        if (rotateValue > 662)
+        {
+            transform.Rotate(new Vector3(0, 0, -20 * speed * Time.deltaTime));
+        }
+        //Rotates the speaker counter-clockwise.
+        if (rotateValue < 362)
+        {
+            transform.Rotate(new Vector3(0, 0, +20 * speed * Time.deltaTime));
+        }
+        
         //Pivots the speaker clockwise.
         if (Input.GetKey("d"))
         {
